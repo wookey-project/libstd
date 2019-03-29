f4d84b5737636becfa7416a2db863d56081c0d78
Philippe Thierry <phil@reseau-libre.net>
2019-03-29 10:51:13 +0100

    [enhancement] make rid of compiler <stdarg.h>, replaced with libstd one. No more dependency on host compiler header files

--- api/stdarg.h
+++ api/stdarg.h
@@ -21,74 +21,28 @@
  * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
-#ifndef STDARGS_H_
-#define STDARGS_H_
+#ifndef LIBSTD_STDARG_H_
+#define LIBSTD_STDARG_H_
 
-/*******************************************
- * Implementation of POSIX stdio
- *******************************************/
-
-
-/****************************************
- * stdargs vprintf() familly
- *
- * This implementation of printf implement a subset
- * of the POSIX.1-2001 and C99 standard API (as we are in an embedded system)
- * Although, the behavior of the functions for the supported flags chars
- * and lenght modifiers is conform to the standard.
- *
- * The following is supported:
- *
- * flag characters:
- *
- * '0'   the value should be zero-padded. This flag is allowed for any
- *       numerical value, including pointer (p).
- *       This flag *must* be followed by a numerical, decimal value specifying
- *       the size to which the value must be padded.
- *
- *  length modifier:
- *
- *  'l'      long int conversion
- *  'll'     long long int conversion
- *  'd','i'  integer signed value conversion
- *  'u'      unsigned integer value conversion
- *  'h'      short int conversion
- *  'hh'     unsigned char conversion
- *  'x'      hexadecimal value, mapped on a long integer conversion
- *  'o'      octal value, mapped on a long integer conversion
- *  'p'      pointer, word-sized unsigned integer, starting with 0x and padded
- *           with 0 up to word-length size
- *  'c'      character value conversion
- *  's'      string conversion
- *
- *  other flags characters and length modifiers are not supported,
- *  generating an immediate stop of the fmt parsing.
- *
- * Conforming (for the supported flags and length modifier and error return)
- * to:
- * POSIX.1-2001, POSIX.1-2008, C89, C99
- *
- */
+#define _STDARG_H
+#define _ANSI_STDARG_H_
 
 /*
- * \brief formatted printing to the standard console using va_list.
- *
- * Printing is synchronous, including a syscall execution.
- *
- * \param fmt the formated string to print
- * \return the number of characters printed on success, -1 on failure
+ * ISO C Standard:  7.15  Variable arguments  <stdarg.h>
  */
-int vprintf(const char *fmt, va_list args);
 
-/*
- * formatted printing to a given buffer, printing at most len chars, using va_list,
- * including the terminating character into dst.
- */
-int vsnprintf(char *dst, size_t len, const char *fmt, va_list args);
+#include "api/types.h"
+/* va_list definition is needed */
+#ifndef __GNUC_VA_LIST
+#define __GNUC_VA_LIST
+typedef __builtin_va_list __gnuc_va_list;
+#endif
 
-/*
- * formatted printing to a given buffer, using va_list.
- */
-int vsprintf(char *dst, const char *fmt, va_list args);
+typedef __gnuc_va_list va_list;
 
-#endif/*STDARGS_H_*/
+#define va_start(v,l)	__builtin_va_start(v,l)
+#define va_end(v)	__builtin_va_end(v)
+#define va_arg(v,l)	__builtin_va_arg(v,l)
+
+
+#endif/*LIBSTD_STDARG_H_*/
--- api/stdio.h
+++ api/stdio.h
@@ -24,7 +24,7 @@
 #ifndef STDIO_H_
 #define STDIO_H_
 
-#include <stdarg.h>
+#include "api/stdarg.h"
 #include "api/types.h"
 
 /*******************************************
@@ -93,5 +93,69 @@ int snprintf(char *dst, size_t len, const char *fmt, ...);
  * formatted printing to a given buffer.
  */
 int sprintf(char *dst, const char *fmt, ...);
+
+/****************************************
+ * stdargs vprintf() familly
+ *
+ * This implementation of printf implement a subset
+ * of the POSIX.1-2001 and C99 standard API (as we are in an embedded system)
+ * Although, the behavior of the functions for the supported flags chars
+ * and lenght modifiers is conform to the standard.
+ *
+ * The following is supported:
+ *
+ * flag characters:
+ *
+ * '0'   the value should be zero-padded. This flag is allowed for any
+ *       numerical value, including pointer (p).
+ *       This flag *must* be followed by a numerical, decimal value specifying
+ *       the size to which the value must be padded.
+ *
+ *  length modifier:
+ *
+ *  'l'      long int conversion
+ *  'll'     long long int conversion
+ *  'd','i'  integer signed value conversion
+ *  'u'      unsigned integer value conversion
+ *  'h'      short int conversion
+ *  'hh'     unsigned char conversion
+ *  'x'      hexadecimal value, mapped on a long integer conversion
+ *  'o'      octal value, mapped on a long integer conversion
+ *  'p'      pointer, word-sized unsigned integer, starting with 0x and padded
+ *           with 0 up to word-length size
+ *  'c'      character value conversion
+ *  's'      string conversion
+ *
+ *  other flags characters and length modifiers are not supported,
+ *  generating an immediate stop of the fmt parsing.
+ *
+ * Conforming (for the supported flags and length modifier and error return)
+ * to:
+ * POSIX.1-2001, POSIX.1-2008, C89, C99
+ *
+ */
+
+/*
+ * \brief formatted printing to the standard console using va_list.
+ *
+ * Printing is synchronous, including a syscall execution.
+ *
+ * \param fmt the formated string to print
+ * \return the number of characters printed on success, -1 on failure
+ */
+int vprintf(const char *fmt, va_list args);
+
+/*
+ * formatted printing to a given buffer, printing at most len chars, using va_list,
+ * including the terminating character into dst.
+ */
+int vsnprintf(char *dst, size_t len, const char *fmt, va_list args);
+
+/*
+ * formatted printing to a given buffer, using va_list.
+ */
+int vsprintf(char *dst, const char *fmt, va_list args);
+
+
 
 #endif/*!STDIO_H_*/
