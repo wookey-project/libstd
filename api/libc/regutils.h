70d363a38c4f481c7a12dd83b30ddebc1144c1e0
Philippe Thierry <phil@reseau-libre.net>
2020-08-28 15:35:47 +0200

    [bugfix] typo in non-FramaC use case

--- api/libc/regutils.h
+++ api/libc/regutils.h
@@ -121,8 +121,10 @@ __INLINE int8_t set_reg_value(register_t reg, uint32_t value,
                               uint32_t mask, uint8_t pos)
 {
     uint32_t tmp;
+#ifdef __FRAMAC__
     uint64_t tmp1;
     uint32_t tmp2;
+#endif
 
 
     if (pos > 31)
