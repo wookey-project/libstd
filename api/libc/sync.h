bc07967ac30e9166297e04643d46bc320ecf4652
Philippe Thierry <phil@reseau-libre.net>
2020-08-28 16:09:22 +0200

    [enhancement] add informational comment

--- api/libc/sync.h
+++ api/libc/sync.h
@@ -12,6 +12,22 @@
  * TODO: is arch_data_membarrier() not enough here ? instead of memsync.
  */
 
+/*
+ * These functions permits to ensure that target data are written back in memory before
+ * the next instruction happens. This avoid optimization side-effects (typically register
+ * caching) when concurrent threads share a given variable.
+ *
+ * CAUTION: these functions permits to ensure that the given variables (scalar, atomically written
+ * in memory (reduced to 32bit scalar types and smaller ones) are correctly pushed back in memory
+ * in order to be consistent in the case of a separately threaded (concurrent) accessor.
+ *
+ * These functions DOES NOT protect consistency for non-atomic types (structures or any type that
+ * are *not* atomically written in the corresponding hardware datasheet.
+ * To resolve this problematic, please use mutexes and semaphores. In the same way, mutexes and
+ * semaphore *does not* protect against compiler optimization and memory barriers myst be used
+ * in addition.
+ */
+
 /*@
 	@ requires \valid(target);
 	@ assigns *target ;
@@ -44,5 +60,17 @@ inline void set_u32_with_memsync(uint32_t *target, uint32_t val) {
     *target = val;
     arch_data_memsync();
 }
+
+/*@
+	@ requires \valid(target);
+	@ assigns *target ;
+	@ ensures *target == val ;
+*/
+inline void set_bool_with_memsync(bool *target, bool val) {
+    /* let the effective assignation be compiled here */
+    *target = val;
+    arch_data_memsync();
+}
+
 
 #endif/*__SYNC_H_*/
